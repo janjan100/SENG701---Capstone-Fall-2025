a07c7f3957373771c9c2ef5c070efc65c8049859
Janell Forchu <jforchu@gmail.com>
2025-10-27 13:50:52 -0400

    readded print extensions metadata

--- src/background.js
+++ src/background.js
@@ -1,8 +1,9 @@
- 
-chrome.management.getAll((extensions) => { //uses managament API. Cannot connect directly. Need getAll()
+ //uses managament API. Cannot connect directly. Need getAll()
+chrome.management.getAll((extensions) => {
   extensions.forEach(ext => {
     console.log(ext.name, ext.version, ext.enabled, ext.permissions);
-     //prints meta data about installed extensions. Needs array to print
-
   });
 });
+     //prints meta data about installed extensions. Needs array to print
+     // 
+    
