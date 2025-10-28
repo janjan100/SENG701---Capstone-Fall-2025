577b82c9609908980ef407b5518c4ef42a67c916
Janell Forchu <jforchu@gmail.com>
2025-10-28 13:13:47 -0400

    added style css sheet

--- src/background.js
+++ src/background.js
@@ -1,9 +1,19 @@
  //uses managament API. Cannot connect directly. Need getAll()
+
+
+
+
 chrome.management.getAll((extensions) => {
   extensions.forEach(ext => {
     console.log(ext.name, ext.version, ext.enabled, ext.permissions);
   });
 });
+ 
+
+ 
+
+
+
      //prints meta data about installed extensions. Needs array to print
      // 
     
