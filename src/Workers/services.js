9d82476d0abf269a96749e16c4df4b18afc26942
janjan100 <janellforchu@yahoo.commdjiwd% git config --global user.name janjan100>
2025-11-18 20:40:50 -0500

    Event listener for color change

--- src/Workers/services.js
+++ src/Workers/services.js
@@ -1,16 +1,18 @@
 console.log("Please check below to  console to view metadata about extensions and values. Thank you. ")
 
 
-const button = document.getElementById('myBtn');
-button.addEventListener("click",changeColor);
-
-function changeColor(){
-document.body.style.backgroundColor = 'lightblue';
+// Button 1
+const element1 = document.getElementById('myBtn1');
+element1.addEventListener("click", changeColor1);
+ 
+function changeColor1() {
+    document.body.style.backgroundColor = 'purple';
 }
-
-const dictionaries = document.getElementById('myBtn3')
-function addLink(){
-
+ 
+// Button 2
+const element2 = document.getElementById('myBtn2');
+element2.addEventListener("click", changeColor2);
+ 
+function changeColor2() {
+    document.body.style.backgroundColor = 'red';
 }
-dictionaries.addEventListener("click", )
-d
