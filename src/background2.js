c5e45ae23b17d6f64b51d990c71a08f50b47d01a
janjan100 <janellforchu@yahoo.commdjiwd% git config --global user.name janjan100>
2025-11-19 20:52:21 -0500

    Added event listeners

--- src/background2.js
+++ src/background2.js
@@ -1,15 +1,22 @@
-const element = document.getElementById('myBtn1');
-element.addEventListener("click",changeColor1);
-function changeColor1(){
-document.body.style.backgroundColor = 'purple';
+const element1 = document.getElementById('myBtn1');
+element1.addEventListener("click",lightColor);
+function lightColor(){
+document.body.style.backgroundColor = "rgba(200, 182, 159, 1)";
+document.body.style.color = "black";
 }
-
 
 //function that changes background color when clicked 
 
-
 const element2 = document.getElementById('myBtn2');
-element2.addEventListener("click",changeSize);
+element2.addEventListener("click",darkColor);
+function darkColor(){
+document.body.style.backgroundColor = "#141479ff";
+document.body.style.color = "white";
+}
+
+const element3 = document.getElementById('myBtn3');
+element3.addEventListener("click",changeSize);
 function changeSize(){
 document.body.style.fontSize = "large";
 }
+
