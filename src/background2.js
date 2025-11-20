fdd2cc86302897fff5570028d2338f53130440c0
janjan100 <janellforchu@yahoo.commdjiwd% git config --global user.name janjan100>
2025-11-19 19:48:58 -0500

    Added size increase event listener

--- src/background2.js
+++ src/background2.js
@@ -3,3 +3,13 @@ element.addEventListener("click",changeColor1);
 function changeColor1(){
 document.body.style.backgroundColor = 'purple';
 }
+
+
+//function that changes background color when clicked 
+
+
+const element2 = document.getElementById('myBtn2');
+element2.addEventListener("click",changeSize);
+function changeSize(){
+document.body.style.fontSize = "large";
+}
