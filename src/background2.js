54372b7054ab20f9e7a94e1ef684c17021f456c3
janjan100 <janellforchu@yahoo.commdjiwd% git config --global user.name janjan100>
2025-11-21 23:08:34 -0500

    Added button4 element

--- src/background2.js
+++ src/background2.js
@@ -1,5 +1,5 @@
-const element1 = document.getElementById('myBtn1');
-element1.addEventListener("click",lightColor);
+const button1 = document.getElementById('myBtn1');
+button1.addEventListener("click",lightColor);
 function lightColor(){
 document.body.style.backgroundColor = "rgba(200, 182, 159, 1)";
 document.body.style.color = "black";
@@ -7,8 +7,8 @@ document.body.style.color = "black";
 
 //function that changes background color when clicked 
 
-const element2 = document.getElementById('myBtn2');
-element2.addEventListener("click",darkColor);
+const button2 = document.getElementById('myBtn2');
+button2.addEventListener("click",darkColor);
 function darkColor(){
 document.body.style.backgroundColor = "#141479ff";
 document.body.style.color = "white";
@@ -19,4 +19,6 @@ element3.addEventListener("click",changeSize);
 function changeSize(){
 document.body.style.fontSize = "large";
 }
+
+const element4 = document.getElementById()
 
