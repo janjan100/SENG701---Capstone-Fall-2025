6af0d70d031efc72cc4ad2a09db38d2d47575424
janjan100 <janellforchu@yahoo.commdjiwd% git config --global user.name janjan100>
2025-11-22 21:39:41 -0500

    Remove Addeventlistener

--- src/background2.js
+++ src/background2.js
@@ -1,33 +1,7 @@
 
 
-/*const button1 = document.getElementById('myBtn1');
-button1.addEventListener("click",lightColor);
-function lightColor(){
-document.body.style.backgroundColor = "rgba(200, 182, 159, 1)";
-document.body.style.color = "black";
-}
-*/
-//function that changes background color when clicked 
-
-/*const button2 = document.getElementById('myBtn2');
-//button2.addEventListener("click",darkColor);
-function darkColor(){
-document.body.style.backgroundColor = "#141479ff";
-document.body.style.color = "white";
-}*/
-//
-/*let LargeText
-const element3 = document.getElementById('myBtn3');
-element3.addEventListener("click",changeSize);
-function changeSize(){
-document.body.style.fontSize = "large";
-}
-
-*/
-
-//
 let darkMode = false;
-const button2 = document.getElementById('myBtn2');
+const button2 = document.getElementById('myBtn3');
 button2.addEventListener("click", toggleDarkMode);
 function toggleDarkMode() {
     darkMode = !darkMode; // flips between true/false
@@ -42,7 +16,7 @@ function toggleDarkMode() {
 
 
 let largeText = false;
-const element3 = document.getElementById('myBtn3');
+const element3 = document.getElementById('myBtn2');
 element3.addEventListener("click", toggleFontSize);
 function toggleFontSize(){
     largeText = !largeText;
