19d7c638604c5c0e19f4f7c91ded04be76fae6ce
Janell Forchu <jforchu@gmail.com>
2025-11-23 21:08:03 -0500

    Changed color background

---
+++ src/Toogles.js
@@ -0,0 +1,30 @@
+
+
+let darkMode = false;
+const button2 = document.getElementById('myBtn3');
+button2.addEventListener("click", toggleDarkMode);
+function toggleDarkMode() {
+    darkMode = !darkMode; // flips between true/false
+    if (darkMode) {
+        document.body.style.backgroundColor = "#202029ff";
+        document.body.style.color = "white";
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    }
+}
+
+
+let largeText = false;
+const element3 = document.getElementById('myBtn2');
+element3.addEventListener("click", toggleFontSize);
+function toggleFontSize(){
+largeText = !largeText;
+ if (largeText) {
+document.body.style.fontSize = "large";
+ } else 
+    {
+    document.body.style.fontSize = "small"; // resets to default
+    }
+}
+
