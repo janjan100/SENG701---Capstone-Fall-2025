9941582d1fd08a9e245136c2803f5ea34f383272
janjan100 <janellforchu@yahoo.com>
2025-12-06 17:29:40 -0500

    Added webscraping function but not working using node.js

--- src/Services/background.js
+++ src/Services/background.js
@@ -75,7 +75,7 @@ navigator.userAgentData.getHighEntropyValues(fields)
   // will print low entrophy hints
   console.log("These are the low entropy hint of your browser:", navigator.userAgentData.brands);
 
-  
+
 const opt = {
 type: "basic",
   iconUrl: "Images/cookie_icon.png",
@@ -86,3 +86,18 @@ type: "basic",
 }
 
 
+//
+import puppeteer from 'puppeteer'; 
+
+async function scrapeBrowser(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.goto(url);
+    await page.goto(url);
+     const [el] = await page.$x('//*[@id="yDmH0d"]/c-wiz[5]/div/div/main/div/div[1]/div[2]');
+    const src = await el.getProperty('src');
+    const srcTxt = await src.jsonValues();
+    console.log({srcTxt});
+    browser.close();
+}
+ scrapeBrowser('https://chromewebstore.google.com/category/extensions?utm_campaign=21676428212&utm_content=167305930939&utm_medium=cpc&utm_source=google&utm_term=chrome%20extension');
