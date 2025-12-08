551ffe484895165cdb4365c4fab368b3e641ff3f
janjan100 <janellforchu@yahoo.com>
2025-12-07 22:55:34 -0500

    Added chrome.runtime.SendMessage API

--- src/Scripts/popup.js
+++ src/Scripts/popup.js
@@ -1,5 +1,13 @@
+
+// button and event listener that responses with get action data and data from console
 document.getElementById("myBtn").addEventListener("click", () => {
-chrome.runtime.sendMessage({ action: "getData" }, (response) => {
-document.getElementById("output").innerHTML = response.message;
-});
+  chrome.runtime.sendMessage({ action: "getData" }, (response) => {
+    if (response.success) { // if response is true, print these values and add to HTML document / webpage 
+      document.getElementById("output").innerText = 
+        "Third-party cookies enabled: " + response.thirdPartyCookies; // this one works 
+         "WebRTCIPolicies enabled: " + response.webpRTCIPolicies;
+         "PassingAaving enabled: " + response.passwordSaving; 
+
+    }
+  });
 });
