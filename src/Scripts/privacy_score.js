b5f32b07b69177b24a939cd7334cee5781438a3d
janjan100 <janellforchu@yahoo.com>
2025-12-08 03:36:55 -0500

    Revised privacy score to privacyData1 to avoid issue

--- src/Scripts/privacy_score.js
+++ src/Scripts/privacy_score.js
@@ -1,38 +1,39 @@
 
-function calculatePrivacyScore(privacyData) { //Privacy Score function
+function calculatePrivacyScore(privacyData1) { //Privacy Score function
     let score = 100;
 
     // Check third-party cookies
-    if (privacyData.thirdPartyCookiesAllowed === true) {
+    if (privacyData1.thirdPartyCookiesAllowed === true) {
         score -= 15;
     }
 
     // Check WebRTC IP policy
-    if (privacyData.webRTCIPHandlingPolicy === 'default') {
+    if (privacyData1.webRTCIPHandlingPolicy === 'default') {
         score -= 10; // IP can leak
     }
 
     // Check password saving
-    if (privacyData.passwordSavingEnabled === true) {
+    if (privacyData1.passwordSavingEnabled === true) {
         score -= 20; // Anyone can access your passwords on your local device if they have it or if compromised
     }
 
     // Check safe browsing
-    if (privacyData.safeBrowsingEnabled === false) {
+    if (privacyData1.safeBrowsingEnabled === false) {
         score -= 20; // If enabled, blocks popups of browser, and detects phishings attacks and fake websites
     }
      // Check if doNotTrack is on
-    if(privacyData.doNotTrackEnabled === false){
+    if(privacyData1.doNotTrackEnabled === false){
       score -=15;
     }   return score;
 }
 // Object for yourPrivacyData based on current value of browser values. Excluded high entropy values
-const yourPrivacyData ={
+const yourPrivacyData1 ={
     webRTCIPHandlingPolicy: "default",
     passwordSavingEnabled: true,
     thirdPartyCookiesAllowed: true, 
     safeBrowsingEnabled: true, 
     doNotTrackEnabled: false, 
 };
-let result = calculatePrivacyScore(yourPrivacyData);
+let result = calculatePrivacyScore(yourPrivacyData1);
 console.log('Your browser has this privacy Score: ' + result + ' (excluding high entropy surfaces value). ' + ' Please take actions based on recommendations. Thank you for using.');
+ 
