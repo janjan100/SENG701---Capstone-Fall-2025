2898a2c9ac7587da5374c37a1dfde48fbbbb41e4
janjan100 <janellforchu@yahoo.com>
2025-12-09 16:38:17 -0500

    Adding innerText elements

--- src/Scripts/popup.js
+++ src/Scripts/popup.js
@@ -4,7 +4,10 @@ document.getElementById("myBtn").addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "getData" }, (response) => {
     if (response.success) { // if response is true, print these values and add to HTML document / webpage 
       document.getElementById("output").innerText = 
-        "Third-party cookies enabled: " + response.thirdPartyCookies; // this one works 
+        "Here are enabled privacy key toggles. Third-party cookies enabled: " + response.thirdPartyCookies  + " Do Not tracket enabled: "+response.doNotTrackEnabled;
+
+       
+      
     
 
     }
