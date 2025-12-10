c5b76128aacc735b976594070f9800211dbd238d
janjan100 <janellforchu@yahoo.com>
2025-12-09 19:44:31 -0500

    Completed innerText values(prints key privacy setting with breakline)

--- src/Scripts/popup.js
+++ src/Scripts/popup.js
@@ -1,16 +1,16 @@
 
-// button and event listener that responses with get action data and data from console
+// Button and event listener that responses with get action data and data from console
 document.getElementById("myBtn").addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "getData" }, (response) => {
     if (response.success) { // if response is true, print these values and add to HTML document / webpage 
-      document.getElementById("output").innerText = 
-        "Here are enabled privacy key toggles. Third-party cookies enabled: " + response.thirdPartyCookies  + " Do Not tracket enabled: "+response.doNotTrackEnabled;
-response.doNotTrackEnabled response.passwordSaving response.webpRTCIPolicies response.safeBrowsing
-       
-       
-      
-    
-
-    }
+      document.getElementById("output").innerText = //defined values and innerText values
+      `Here are the privacy security states
+      Third-party cookies Setting enabled:  ${response.thirdPartyCookies}.
+       Do Not track Setting enabled: ${response.doNotTrackEnabled}.
+       PassSaving Setting enabled: ${response.passwordSaving}. 
+       WebpRTCIPolicies Setting enabled: ${response.webpRTCIPolicies}. 
+       Safe Browsing Setting enabled: ${response.safeBrowsing}.`
+    } // .innerText: Prints Plain text print on the UI.This is safe way to print!!
+    // Seperate each line for break line. Easy run to break line. 
   });
 });
